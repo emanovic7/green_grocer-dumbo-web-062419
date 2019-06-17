11266ed87c2cb7669a29fccea1ce3ddf0eb6b813
emanovic7 <e_owusu@live.com>
2019-06-17 17:15:08 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,25 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  #create return hash with #each_with_object
+  cart.each_with_object({}) do |item, items_cart| #(return hash)
+    item.each do |key, value|
+      #binding.pry
+      if items_cart[key] #if the key exists in the return hash
+        value[:count] += 1 #increase the attribute
+      else
+        value[:count] = 1 #else, initialize the attribute at 1
+        items_cart[key] = value #set cart values
+      end
+      #binding.pry
+    end
+
+  end
+
 end
+
+
+
 
 def apply_coupons(cart, coupons)
   # code here
@@ -13,3 +32,14 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+
+
+# def sample
+#   [:foo, :bar, :jazz].each_with_object({}) do |item, hash|
+#     hash[item] = item.to_s.upcase
+#   end
+#   binding.pry
+# end
+#
+# sample
